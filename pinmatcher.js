2a05ca1ab9ab88cd03c7da4c6dd7da59db3e35a2
sumonhasan55 <wubsumon7577@gmail.comm>
2023-02-08 19:46:19 +0600

    pin-cheack-and allert set

--- pinmatcher.js
+++ pinmatcher.js
@@ -2,20 +2,17 @@ function getPin() {
     const Pin =  codeGenarator();
     const pinString = Pin + '';
     if (pinString.length === 4) {
-        return Pin;
-        
+        return Pin; 
     } else {
         return codeGenarator();
-        
     }
-    
 };
 
 
 function codeGenarator() {
     const number = Math.round(Math.random()*10000)
     return number;
-}
+};
 
 document.getElementById('generator-btn').addEventListener('click',function(){
     const pin =getPin()
@@ -42,18 +39,37 @@ document.getElementById('calculator').addEventListener('click', function(event){
             digits.pop();
             const remainingDigits = digits.join('');
             verifyPinField.value= remainingDigits;
-           
         }
-        
     }
         
     else {
-
-       
         const NewTypedNumber = PreviousTypedNumber + number;
         verifyPinField.value = NewTypedNumber;
-        
     }
      
-
 });
+//pin cheack 
+
+document.getElementById('submit-btn').addEventListener('click', function(){
+  const generatePinField = document.getElementById('display-pin');
+  const generatePin = generatePinField.value;
+
+  const typedPinField = document.getElementById('verify-pinfield');
+  const typedPin = typedPinField.value;
+
+  if (generatePin === typedPin) {
+   alert('✅ &nbsp; Pin Matched... Secret door is opening for you')
+
+    
+  } else {
+    const Failuremsg = document.getElementById('notify-failure');
+    console.log(Failuremsg)
+   alert('❌ &nbsp; Pin Didn t Match, Please try again')
+   
+  }
+  generatePinField.value= '';
+  typedPinField.value = '';
+});
+
+
+
